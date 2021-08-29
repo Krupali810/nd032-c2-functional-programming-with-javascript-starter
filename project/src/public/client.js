807541ccf3f2560c14f8076f58370c074da6a317
Krupali Patel <krupali810@yahoo.com>
2021-08-29 10:49:02 -0400

    Added Immutable JS List

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,8 +1,8 @@
 let store = {
-  user: { name: "Student" },
+  user: { name: "User" },
   roverInfo: "",
   roverInfoMap: {},
-  rovers: ["Curiosity", "Opportunity", "Spirit"],
+  rovers: Immutable.List(["Curiosity", "Opportunity", "Spirit"]),
   selectedRover: "Curiosity".toLowerCase(),
 };
 
@@ -23,11 +23,16 @@ const App = ({ rovers, roverInfo, selectedRover }) =>
   `
           <header></header>
           <main>
+            <div style={{alignContent:"center"}}>
               ${Greeting(store.user.name)}
-              <section>
+            </div>
+              <div style="width:100%; margin:0 auto;">
                 <nav>
                   ${roverNavTabs(rovers)}
                 </nav>
+              </div>
+              <section>
+                <h1>Viewing ${store.selectedRover.toUpperCase()} Rover Photos</h1>
               </section>
               <section>
                   ${roverInformation(selectedRover, roverInfo)}
@@ -52,20 +57,44 @@ const Greeting = (name) => {
       `;
 };
 
-const roverNavTabs = (rovers) =>
-  rovers.map(
-    (roverName) => `
+const roverNavTabs = (rovers) => {
+  console.log(`rovers`, rovers.toJS());
+  return rovers.toJS().map((roverName) => createLinks(roverName));
+};
+
+const createLinks = (roverName) => {
+  return `
+    <button id=${roverName} type="button" class="btn btn-secondary" onClick="selectRover(id)">
+      ${roverName}
+    </button>
+  `;
+};
+
+const roverLinks = (rovers) =>
+  rovers.map((roverName) => createNavLinks(roverName));
+
+const createNavLinks = (roverName) => {
+  return `
+  <li class="nav-item" onClick="selectRover(id)">
+    <a class="nav-link" >${roverName}</a>
+  </li>
+  `;
+};
+
+const createButton = (roverName) => {
+  return `
     <button id=${roverName} onClick="selectRover(id)">
       ${roverName}
     </button>
-  `
-  );
+  `;
+};
 
 function selectRover(roverName) {
   updateStore(store, { selectedRover: roverName.toLowerCase() });
 }
 
 const roverInformation = (rover, roverInfo) => {
+  console.log(`rover store info`, store.roverInfoMap);
   const isRoverInfoPresent = Object.keys(store.roverInfoMap).find(
     (key) => key === store.selectedRover
   );
@@ -83,13 +112,19 @@ const roverInformation = (rover, roverInfo) => {
   return `<div/>`;
 };
 
+const createImage = (imgSrc) => {
+  return `
+    <img src=${imgSrc} height="350px" width="30%"/>
+ `;
+};
+
 const displayRoverPhotoGrid = (photosArray) => {
-  const divForPhotosArray = photosArray.map(
-    (val) => `<img src=${val} height="350px" width="30%"/>`
-  );
+  const divForPhotosArray = photosArray.map((val) => createImage(val));
 
   return divForPhotosArray;
 };
+
+const test = () => {};
 
 const getDataFromAPI = (url) => {
   let roverName = store.selectedRover;
@@ -97,8 +132,12 @@ const getDataFromAPI = (url) => {
     fetch(`http://localhost:3000/${url}/${roverName}`)
       .then((res) => res.json())
       .then((roverInfo) => {
+        console.log(`roverInfo is`, roverInfo);
         updateStore(store, {
-          roverInfoMap: { ...store.roverInfoMap, [`${roverName}`]: roverInfo },
+          roverInfoMap: {
+            ...store.roverInfoMap,
+            [`${roverName}`]: roverInfo,
+          },
         });
       })
       .catch((err) => console.log(err));
